c12a0915580d395cd54976ac71cdbb7b8d3a620e
Eli Citron <ebcitron@gmail.com>
2019-05-10 11:31:08 -0500

    Projects done

--- actionRouter.js
+++ actionRouter.js
@@ -7,14 +7,69 @@ const actionRouter = express.Router();
 
 // Get all
 
-//Get actions by project_id
+//Get actions by action_id
 
-//Post new action to specific project by project_id
+//Post new action to specific action by action_id
 
 //Put action by id
 
 //Delete action by ID
 
+// Get all
+
+actionRouter.get('/', async(req, res) => {
+    try{
+        const action = await Action.get();
+        res.status(200).json({action});
+    } catch( error ) {
+        res.status(500).json({message: "Error retrieving actions"});
+    }
+});
+
+//Get actions by action_id
+
+actionRouter.get('/:id', async (req,res) => {
+    try{
+        const action = await Action.get(req.params.id);
+        res.status(200).json({action});
+    } catch(error){
+        res.status(500).json({ message: "Error retrieving action"});
+    }
+});
+
+//Post new action
+
+actionRouter.post('/:project_id', async (req,res) => {
+    try{
+        const action = await Action.insert(req.params.project_id, req.body);
+        res.status(200).json({action});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: "Error adding action"});
+    }
+});
+
+//Put action by ID
+
+actionRouter.put('/:id', async( req,res) => {
+    try{
+        const action = await Action.update(req.params.id, req.body);
+        res.status(200).json({action});
+    } catch (error) {
+        res.status(500).json({message : "Error editing action"});
+    }
+});
+
+//Delete action by ID
+
+actionRouter.delete('/:id', async (req, res) => {
+    try{
+        const action = await Action.remove(req.params.id);
+        res.status(200).json({message: "action was deleted"});
+    } catch (error){
+        res.status(500).json({message: "Error deleting action"});
+    }
+});
 
 
 module.exports = actionRouter;
